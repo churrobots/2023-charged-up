236998555529c25ff67cf840eae739bcb95b245d
EPA Robotics <55808130+epa-robotics-team@users.noreply.github.com>
2023-02-04 15:03:39 -0800

    (maynor) Updated rev Library, added new slew
    Co-Authored-By: MaynorB <46939742+MaynorB@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -167,6 +167,11 @@ public final class Constants {
     public static final double kPModuleTurningController = 1;
 
     public static final double kPModuleDriveController = 1;
+
+    public static final double kDirectionSlewRate = 1.2; // radians per second
+    public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%)
+    public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
+
   }
 
   public static final class OIConstants {
