c726f4f0416d286ea4122bdcc2037ea251c7d69e
EPA Robotics <55808130+epa-robotics-team@users.noreply.github.com>
2023-02-07 22:17:20 -0800

    apply deadbanding to the inputs

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -57,9 +57,9 @@ public class RobotContainer {
         new RunCommand(
             () -> m_robotDrive.drive(
                 // These are intentionally inverted and reversed yay
-                -m_driverController.getLeftX(),
-                m_driverController.getLeftY(),
-                -m_driverController.getRightX(),
+                -MathUtil.applyDeadband(m_driverController.getLeftX(), 0.1),
+                MathUtil.applyDeadband(m_driverController.getLeftY(), 0.1),
+                -MathUtil.applyDeadband(m_driverController.getRightX(), 0.1),
                 true),
             m_robotDrive));
 
